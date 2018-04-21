f23d43b306850042e8cbb49cdd3b370542290fb8
theonak4 <league@WTS14-4.attlocal.net>
2018-04-21 15:55:31 -0700

    string methods 2

--- src/StringMethods2.java
+++ src/StringMethods2.java
@@ -1,28 +1,26 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
 public class StringMethods2 {
 	public static void main(String[] args) {
-		
+
 		// 1. Create a String variable and initialize it with
-		//    random upper and lower case characters.
-String Sophia="Sophia";
+		// random upper and lower case characters.
+		String Sophia = "Sophia";
 		// 2. Print your String to the console in upper case.
 		System.out.println(Sophia.toUpperCase());
 		// 3. Print your String to the console in lower case.
-System.out.println(Sophia.toLowerCase());
+		System.out.println(Sophia.toLowerCase());
 		// 4. Print the first 3 char's of your String
-		//    HINT: .substring(start,end)
-		System.out.println(Sophia.substring(0,3));
+		// HINT: .substring(start,end)
+		System.out.println(Sophia.substring(0, 3));
 		// 5. Print a single char somewhere in the middle of your String
-		//    REMINDER: char's in string start at index 0
+		// REMINDER: char's in string start at index 0
 		System.out.println(Sophia.charAt(3));
 		// 6. BONUS -- print the LAST 3 char's of your string using
-		//        .length() to determine WHERE the last 3 char's are located.
-		for (int i = 0; i < Sophia.length(); i++) {
-			
-		}
-		
+		// .length() to determine WHERE the last 3 char's are located.
+		System.out.println(Sophia.length());
+		System.out.println(Sophia.substring(3, 6));
+
 	}
 }
